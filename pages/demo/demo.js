1afbfebb8f00e5ebe2efa158f2c0d1ba7c807302
Spirit <martinkleinpaste@hotmail.com>
2021-10-28 14:41:52 +0200

    expanded demo module

--- pages/demo/demo.js
+++ pages/demo/demo.js
@@ -6,8 +6,37 @@
 moduleManager.ModuleRespository.demo = function () {
     this.moduleName = "demo"
 
+    this.subModule;
+
+    // an example as to how to access the dom within modules
+    // the rootNode object always points to the element that houses the module
     this.rootNode.querySelector(".demo").innerHTML = "AMF appears to be working correctly";
-    moduleManager.loadJSModule("subDemo", "pages/demo", false, this.id)
+
+    // load a secondary JS script as a module.
+    // it'll be tied to this module when instanced and it'll be tied to this module
+    // the first parameter is the name of the file and module (they need to match!)
+    // the second parameter is the directory where to find the module
+    // the third parameter is a boolean to determine if extra CSS (with matching name) should be loaded.
+    // the fourth parameter is the ID of the parent module
+    // any parameters after the fourth is used as parameters for the constructor function of the module
+    this.subModule = moduleManager.loadJSModule("subDemo", "pages/demo", false, this.id).then(module => {
+        // after the module is loaded and the promise is resolved, you can interact with the module as shown
+        console.log("ping") 
+        console.log(module.pong())
+        return module
+    })
+
+    // this function will be called from the dom
+    // the button you click passes along a value and is entered as a parameter in this function
+    this.domToJSDemo = function (increment)
+    {
+        // in the HTML you can see that you need to use moduleExecute to get to your module's JS code
+        // the first parameter is usually "this" because it is used to determine what object called the function
+        // the second parameter is a STRING CONTAINING THE NAME OF THE FUNCTION
+        // all that comes after the 2nd parameter, is used as parameters for the function you're calling
+        let counter = this.rootNode.querySelector(".click-counter")
+        counter.innerHTML = Number.parseInt(counter.innerHTML) + increment
+    }
 
     return this;
 }
--- pages/demo/subDemo.js
+++ pages/demo/subDemo.js
@@ -2,12 +2,21 @@
 // 22/03/2021
 //
 
-// Append the demo module to the modules the moment this file gets loaded
+// Append the subDemo module to the modules the moment this file gets loaded
 moduleManager.ModuleRespository.subDemo = function () {
     this.moduleName = "subDemo"
 
-    console.log(this)
+    // note: because of how this module is loaded in this demo, it does not have a rootnode, as it's loaded effectively as a slave module.
+
+    // the moment the module is loaded, it'll append some things to a particular class on screen
+    // also demonstrates how a child module can interact with the parent module
+    // this.parentModule can be used to access the parent module
     this.parentModule.rootNode.querySelector(".demo").innerHTML += ` <span style="color:#0f0">also the sub-module works</span>`;
+
+    // a pong function to demonstrate how the modules can interact from parent to child
+    this.pong = function () {
+        return "pong"
+    }
 
     return this
 }
